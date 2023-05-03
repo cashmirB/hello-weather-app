9135cb75e544cf2f80656150160554abcb1f3afb
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-05-03 22:17:05 +0100

    added axios
    import the data from weather api

---
+++ src/Greetings.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default function Greetings(props) {
+  function sendInfo(response) {
+    alert(
+      `It is currently ${response.data.temperature.current}°C here in ${response.data.city} with a humidity of ${response.data.temperature.humidity}`
+    );
+  }
+
+  let apiKey = "77c45f57f94o3fb60fb856c88ed36ata";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(sendInfo);
+  return <h1>Welcome to my weather page!😎</h1>;
+}
